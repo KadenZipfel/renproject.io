62b0cecab23aa7e732cdef545798bba83ab93f40
Vince Au <vinceau09@gmail.com>
2019-03-05 16:45:37 +1100

    Add link to Litepaper under featured section

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -11,6 +11,7 @@ interface FeaturedState {
 
 class Featured extends React.Component<FeaturedProps, FeaturedState> {
     public render(): JSX.Element {
+        const litepaper = require("../styles/images/icons/icon-litepaper.svg");
         return (
             <div className="featured">
                 <div className="container">
@@ -18,6 +19,9 @@ class Featured extends React.Component<FeaturedProps, FeaturedState> {
                         <div className="featured--content">
                             <ContentBlock subtitle="Unstoppable Privacy">
                                 <p>Transfer tokens in zero-knowledge, trustlessly swap between blockchains, trade on a dark pool, build any privacy preserving application. Never reveal anything.</p>
+                                <div className="content--links">
+                                    <a className="content--link" href="litepaper.pdf" target="_blank" rel="noopener referrer"><img src={litepaper} />Read the Litepaper</a>
+                                </div>
                             </ContentBlock>
                         </div>
                     </div>
