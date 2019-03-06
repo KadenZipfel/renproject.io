31fa4870ac7060c0b15e167fb7759b1418a09176
Vince Au <vinceau09@gmail.com>
2019-03-06 11:28:12 +1100

    Add Follow us on Twitter button to Featured section

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 import ContentBlock from "./ContentBlock";
 
 interface FeaturedProps {
@@ -20,7 +23,8 @@ class Featured extends React.Component<FeaturedProps, FeaturedState> {
                             <ContentBlock subtitle="Unstoppable Privacy">
                                 <p>Transfer tokens in zero-knowledge, trustlessly swap between blockchains, trade on a dark pool, build any privacy preserving application. Never reveal anything.</p>
                                 <div className="content--links">
-                                    <a className="content--link" href="litepaper.pdf" target="_blank" rel="noopener referrer"><img src={litepaper} />Read the Litepaper</a>
+                                    <a className="content--link litepaper" href="litepaper.pdf" target="_blank" rel="noopener referrer"><img src={litepaper} />Read the Litepaper</a>
+                                    <a className="content--link twitter" href="https://twitter.com/renprotocol" target="_blank" rel="noopener referrer"><span className="twitter--icon"><FontAwesomeIcon icon={faTwitter} /></span><span className="twitter--text">Follow us on Twitter</span></a>
                                 </div>
                             </ContentBlock>
                         </div>
