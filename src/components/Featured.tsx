2b0f848930848e7895fbd2f3b5449102976ca694
Vince Au <vinceau09@gmail.com>
2019-03-20 09:55:07 +1100

    Add Follow us on Reddit button to featured section

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faRedditAlien } from "@fortawesome/free-brands-svg-icons";
 
 import ContentBlock from "./ContentBlock";
 import { REN_URLS } from "../lib/constants";
@@ -25,7 +25,8 @@ class Featured extends React.Component<FeaturedProps, FeaturedState> {
                                 <p>Transfer tokens in zero-knowledge, trustlessly swap between blockchains, trade on a dark pool, build any privacy preserving application. Never reveal anything.</p>
                                 <div className="content--links">
                                     <a className="content--link litepaper" href="litepaper.pdf" target="_blank" rel="noopener referrer"><img src={litepaper} />Read the Litepaper</a>
-                                    <a className="content--link twitter" href={REN_URLS.twitter} target="_blank" rel="noopener referrer"><span className="twitter--icon"><FontAwesomeIcon icon={faTwitter} /></span><span className="twitter--text">Follow us on Twitter</span></a>
+                                    <a className="content--link social twitter" href={REN_URLS.twitter} target="_blank" rel="noopener referrer"><span className="social--icon"><FontAwesomeIcon icon={faTwitter} /></span><span className="social--text">Follow us on Twitter</span></a>
+                                    <a className="content--link social reddit" href={REN_URLS.reddit} target="_blank" rel="noopener referrer"><span className="social--icon"><FontAwesomeIcon icon={faRedditAlien} /></span><span className="social--text">Follow us on Reddit</span></a>
                                 </div>
                             </ContentBlock>
                         </div>
