ba500a61b4dbb54cf276d17b19736b0a47549b55
Vince Au <vinceau09@gmail.com>
2019-05-14 16:14:08 +1000

    Fix Newsletter on mobile

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -11,11 +11,13 @@ interface NewsletterState {
 export class Newsletter extends React.Component<NewsletterProps, NewsletterState> {
     public render(): JSX.Element {
         return (
-            <div className="newsletter">
-                <h2>Stay in the loop</h2>
-                <p>Be the first to know when RenVM is operational. Or reach out if you have a project idea: <ExternalLink href="mailto:renshift@renproject.io">renshift@renproject.io</ExternalLink></p>
-                <input placeholder="Email address"></input>
-                <button className="featured--button primary">Sign up</button>
+            <div className="newsletter--container">
+                <div className="newsletter">
+                    <h2>Stay in the loop</h2>
+                    <p>Be the first to know when RenVM is operational. Or reach out if you have a project idea: <ExternalLink href="mailto:renshift@renproject.io">renshift@renproject.io</ExternalLink></p>
+                    <input placeholder="Email address"></input>
+                    <button className="featured--button primary">Sign up</button>
+                </div>
             </div>
         );
     }
