67e859735a6ce6c944744d901317c5d8ebd143cd
Vince Au <vinceau09@gmail.com>
2019-05-15 18:06:50 +1000

    Add Github language colour circles

--- src/components/GithubBlock.tsx
+++ src/components/GithubBlock.tsx
@@ -3,6 +3,8 @@ import { GithubRepo } from "../lib/github";
 import { ExternalLink } from "./ExternalLink";
 import { naturalTime } from "@renex/react-components";
 
+import colours from "github-colors";
+
 interface GithubBlockProps {
     repo: GithubRepo;
 }
@@ -22,7 +24,14 @@ export class GithubBlock extends React.Component<GithubBlockProps, GithubBlockSt
                 <ExternalLink href={repo.html_url}>{repo.name} &rarr;</ExternalLink>
                 <p>{repo.description}</p>
                 <div>
-                    <div>{repo.language}</div>
+                    <div className="repo--lang">
+                        <div className="repo--lang--dot">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                                <circle cx="50" cy="50" r="50" fill={colours.get(repo.language).color} />
+                            </svg>
+                        </div>
+                        <span>{repo.language}</span>
+                    </div>
                     <div>{repo.stargazers_count}</div>
                     {repo.license && <div>{repo.license.spdx_id}</div>}
                     <div>Updated {naturalTime(repo.updated_at.getTime() / 1000, { message: "something", suffix: "ago", countDown: false })}</div>
