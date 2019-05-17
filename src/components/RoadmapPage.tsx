24d125813d663270e101d5b7e0ef63ac60044f8a
Vince Au <vinceau09@gmail.com>
2019-05-17 14:10:19 +1000

    Update Roadmap with ecosystem section

--- src/components/RoadmapPage.tsx
+++ src/components/RoadmapPage.tsx
@@ -4,6 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { MediumBannerInstance } from "./MediumBanner";
 import { GithubStats } from "./GithubStats";
+import ContentBlock from "./ContentBlock";
 
 class RoadmapPage extends React.Component {
     public render(): JSX.Element {
@@ -14,6 +15,17 @@ class RoadmapPage extends React.Component {
                     <div className="container" />
                 </div>
                 {MediumBannerInstance}
+                <div className="section section--ecosystem">
+                    <div className="container">
+                        <div className="row">
+                            <div className="section--content">
+                                <ContentBlock subtitle="A growing ecosystem">
+                                    <p>We're dedicated to developing world class privacy and interoperability focused tools and keeping you up-to-date on our progress. Because the blockchain space is constantly evolving, certain features or decisions may change over time.</p>
+                                </ContentBlock>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="section section--story">
                     <div className="container">
                         <div className="row">
