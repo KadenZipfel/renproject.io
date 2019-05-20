688968eda2a771301d72e9fee37a5b94dcdb34f6
Vince Au <vinceau09@gmail.com>
2019-05-20 14:20:03 +1000

    Revert "Attempt to fix basename"
    This reverts commit 65c0af09800e18d1df3236dad1af310dd6ac0976.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,9 +8,8 @@ import RoadmapPage from "./RoadmapPage";
 
 class App extends React.Component {
     public render(): JSX.Element {
-        const basename = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : undefined;
         return (
-            <BrowserRouter basename={basename}>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <>
                     <ScrollToTop />
                     <Route path="/" exact component={HomePage} />
