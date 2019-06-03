435894c1a4dce5992ece6678f74d20a988d04f8e
Vince Au <vinceau09@gmail.com>
2019-06-03 16:34:28 +1000

    Use functional component for Newsletter

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -3,22 +3,12 @@ import * as React from "react";
 import { ExternalLink } from "./ExternalLink";
 import { REN_URLS } from "../lib/constants";
 
-interface NewsletterProps {
-}
-
-interface NewsletterState {
-}
-
-export class Newsletter extends React.Component<NewsletterProps, NewsletterState> {
-    public render(): JSX.Element {
-        return (
-            <div className="newsletter--container">
-                <div className="newsletter">
-                    <h2>Want to integrate RenVM?</h2>
-                    <p>Sign up here and we'll let you know when it's ready!</p>
-                    <ExternalLink className="featured--button primary" href={REN_URLS.typeform}>Sign up</ExternalLink>
-                </div>
-            </div>
-        );
-    }
-}
+export const Newsletter = () => (
+    <div className="newsletter--container">
+        <div className="newsletter">
+            <h2>Want to integrate RenVM?</h2>
+            <p>Sign up here and we'll let you know when it's ready!</p>
+            <ExternalLink className="featured--button primary" href={REN_URLS.typeform}>Sign up</ExternalLink>
+        </div>
+    </div>
+);
