426ace9aa60e2e9b72e14bf633ae7f95473dd756
Vince Au <vinceau09@gmail.com>
2019-06-05 17:57:11 +1000

    Store Github repos

--- src/components/GithubStats.tsx
+++ src/components/GithubStats.tsx
@@ -9,92 +9,50 @@ import { ExternalLink } from "./ExternalLink";
 import { REN_URLS } from "../lib/constants";
 import { naturalTime } from "../lib/conversions";
 import { withTrashable, TrashableReactProps } from "../lib/trashable";
+import { StoreContext } from "../store/context";
 
 interface GithubStatsProps extends TrashableReactProps {
     usernames: string[];
     limit?: number;
 }
 
-interface GithubStatsState {
-    repos: GithubRepo[];
-    ready: boolean;
-    error: boolean;
-}
+const GithubStatsClass = (props: GithubStatsProps) => {
+    const [error, setError] = React.useState(false);
+    const storeContext = React.useContext(StoreContext);
+    if (storeContext === null) {
+        return <></>;
+    }
+    const { githubRepos, setGithubRepos } = storeContext;
 
-class GithubStatsClass extends React.Component<GithubStatsProps, GithubStatsState> {
-    constructor(props: GithubStatsProps) {
-        super(props);
-        this.state = {
-            repos: [],
-            ready: false,
-            error: false,
-        };
+    const fetchAllRepos = async (): Promise<GithubRepo[]> => {
+        const { usernames } = props;
+        return Promise.all(usernames.map(username => fetchRepos(username))).then(l => ([] as GithubRepo[]).concat(...l));
     }
 
-    public async componentDidMount(): Promise<void> {
+    const fetchData = async () => {
+        if (githubRepos.length > 0) {
+            return
+        };
+
         try {
-            const repos = await this.props.registerPromise(this.fetchRepos());
-            this.setState({ repos, ready: true, error: false });
+            const repos = await props.registerPromise(fetchAllRepos());
+            setGithubRepos(repos);
         } catch (err) {
             console.error(err);
-            this.setState({ error: true });
+            setError(true);
         }
     }
 
-    public render(): JSX.Element {
-        const { error, ready, repos } = this.state;
-        const stats = calculateStats(repos);
-        const fetchError = <p>Failed to fetch information from Github. Please try again later.</p>;
-        const loadingMessage = <p>Fetching data from Github...</p>;
-        return (
-            <div className="gh--stats">
-                <h1>Github</h1>
-                {error ? fetchError : !ready ? loadingMessage :
-                    <Tabs forceRenderTabPanel={true}>
-                        <TabList>
-                            <Tab>Overview</Tab>
-                            <Tab>Top Repos</Tab>
-                        </TabList>
+    React.useEffect(() => {
+        fetchData();
+    }, []);
 
-                        <TabPanel>
-                            <div className="gh--stats--overview">
-                                <div>
-                                    <h2>Commit Activity</h2>
-                                    <GithubActivity repos={repos} />
-                                </div>
-                                <div>
-                                    <h2>Last Commit</h2>
-                                    <p>{naturalTime(stats.lastUpdated.getTime() / 1000, { message: "A few seconds ago", suffix: "ago", countDown: false })}</p>
-                                </div>
-                                <div>
-                                    <h2>Stars</h2>
-                                    <GithubStar count={stats.totalStars} />
-                                </div>
-                                <div>
-                                    <h2>Languages</h2>
-                                    <div className="gh--stats--lang">{stats.languages.map(lang => <GithubLanguage key={lang} language={lang} />)}</div>
-                                </div>
-                            </div>
-                            <div className="gh--stats--footer">
-                                <ExternalLink href={REN_URLS.github}>View on Github &rarr;</ExternalLink>
-                            </div>
-                        </TabPanel>
-                        <TabPanel>
-                            <div className="github--top--repos">
-                                {this.filterRepos().map(repo => <GithubBlock key={repo.id} repo={repo} />)}
-                            </div>
-                        </TabPanel>
-                    </Tabs>}
-            </div>
-        );
-    }
-
-    private filterRepos = (): GithubRepo[] => {
-        const { limit } = this.props;
+    const filterRepos = (): GithubRepo[] => {
+        const { limit } = props;
         // Only show the Github repos which were updated within the last month
         const filterDate = new Date();
         filterDate.setMonth(filterDate.getMonth() - 1);
-        let repos = this.state.repos.filter(repo => repo.updated_at > filterDate);
+        let repos = githubRepos.filter(repo => repo.updated_at > filterDate);
 
         // sort by stars
         repos = repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
@@ -105,10 +63,51 @@ class GithubStatsClass extends React.Component<GithubStatsProps, GithubStatsStat
         return repos;
     }
 
-    private fetchRepos = async (): Promise<GithubRepo[]> => {
-        const { usernames } = this.props;
-        return Promise.all(usernames.map(username => fetchRepos(username))).then(l => ([] as GithubRepo[]).concat(...l));
-    }
+    const ready = githubRepos.length > 0;
+    const stats = calculateStats(githubRepos);
+    const fetchError = <p>Failed to fetch information from Github. Please try again later.</p>;
+    const loadingMessage = <p>Fetching data from Github...</p>;
+    return (
+        <div className="gh--stats">
+            <h1>Github</h1>
+            {error ? fetchError : !ready ? loadingMessage :
+                <Tabs forceRenderTabPanel={true}>
+                    <TabList>
+                        <Tab>Overview</Tab>
+                        <Tab>Top Repos</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <div className="gh--stats--overview">
+                            <div>
+                                <h2>Commit Activity</h2>
+                                <GithubActivity repos={githubRepos} />
+                            </div>
+                            <div>
+                                <h2>Last Commit</h2>
+                                <p>{naturalTime(stats.lastUpdated.getTime() / 1000, { message: "A few seconds ago", suffix: "ago", countDown: false })}</p>
+                            </div>
+                            <div>
+                                <h2>Stars</h2>
+                                <GithubStar count={stats.totalStars} />
+                            </div>
+                            <div>
+                                <h2>Languages</h2>
+                                <div className="gh--stats--lang">{stats.languages.map(lang => <GithubLanguage key={lang} language={lang} />)}</div>
+                            </div>
+                        </div>
+                        <div className="gh--stats--footer">
+                            <ExternalLink href={REN_URLS.github}>View on Github &rarr;</ExternalLink>
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="github--top--repos">
+                            {filterRepos().map(repo => <GithubBlock key={repo.id} repo={repo} />)}
+                        </div>
+                    </TabPanel>
+                </Tabs>}
+        </div>
+    );
 
 }
 
--- src/store/context.tsx
+++ src/store/context.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
 import { MediumPost } from "../lib/medium";
+import { GithubRepo } from "../lib/github";
 
 interface StoreInterface {
   mediumPosts: MediumPost[];
+  githubRepos: GithubRepo[];
   setMediumPosts: any;
+  setGithubRepos: any;
 }
 
 export const StoreContext = React.createContext<StoreInterface | null>(null);
 
 export const StoreProvider = (props: any) => {
   const [mediumPosts, setMediumPosts] = React.useState([]);
+  const [githubRepos, setGithubRepos] = React.useState([]);
   const context: StoreInterface = {
     mediumPosts,
     setMediumPosts,
+    githubRepos,
+    setGithubRepos,
   };
   return (
     <StoreContext.Provider value={context}>
