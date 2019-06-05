4d4bf766fc8d5327798b49455f1eb094de1bb962
Vince Au <vinceau09@gmail.com>
2019-06-05 18:23:16 +1000

    Convert Telegram selector into functional component

--- src/components/TelegramSelector.tsx
+++ src/components/TelegramSelector.tsx
@@ -8,39 +8,26 @@ interface TelegramSelectorProps {
     default?: number;
 }
 
-interface TelegramSelectorState {
-    value: string;
-}
+export const TelegramSelector = (props: TelegramSelectorProps) => {
+    const { links } = props;
+    const defaultValue = props.default || 0;
+    const [value, setValue] = React.useState(links[defaultValue].url);
 
-
-export class TelegramSelector extends React.Component<TelegramSelectorProps, TelegramSelectorState> {
-    constructor(props: TelegramSelectorProps) {
-        super(props);
-        const defaultValue = props.default || 0;
-        this.state = {
-            value: props.links[defaultValue].url,
-        };
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setValue(event.target.value);
     }
 
-    public render(): JSX.Element {
-        const { links } = this.props;
-        const { value } = this.state;
-        return (
-            <div className="telegram--button">
-                <div className="telegram--button--container">
-                    <ExternalLink className="button button--alt" href={value}><FontAwesomeIcon icon={faTelegramPlane} pull="left" />Open Telegram</ExternalLink>
-                    <div className="telegram--select">
-                        <select value={value} onChange={this.handleChange}>
-                            {links.map((l, index) => <option key={`telegram-select-${index}-${l.name}`} value={l.url}>{l.name}</option>)}
-                        </select>
-                    </div>
+    return (
+        <div className="telegram--button">
+            <div className="telegram--button--container">
+                <ExternalLink className="button button--alt" href={value}><FontAwesomeIcon icon={faTelegramPlane} pull="left" />Open Telegram</ExternalLink>
+                <div className="telegram--select">
+                    <select value={value} onChange={handleChange}>
+                        {links.map((l, index) => <option key={`telegram-select-${index}-${l.name}`} value={l.url}>{l.name}</option>)}
+                    </select>
                 </div>
             </div>
-        );
-    }
-
-    private handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        this.setState({ value: event.target.value });
-    }
+        </div>
+    );
 
 }
