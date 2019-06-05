b99ef00ec34c0d9bed6f50ab306abc21798cad0c
Vince Au <vinceau09@gmail.com>
2019-06-05 15:47:28 +1000

    Turn RoadmapPage into functional component

--- src/components/RoadmapPage.tsx
+++ src/components/RoadmapPage.tsx
@@ -12,63 +12,61 @@ import { getMilestones } from "../lib/milestone";
 
 const Fade = require("react-reveal/Fade");
 
-class RoadmapPage extends React.Component {
-    public render(): JSX.Element {
-        const speechIcon = require(`../styles/images/icons/icon-speech.svg`);
-        return (
-            <div className="roadmap">
-                <Header />
-                <div className="section section--banner">
-                    <div className="container" />
+export const RoadmapPage = () => {
+    const speechIcon = require(`../styles/images/icons/icon-speech.svg`);
+    return (
+        <div className="roadmap">
+            <Header />
+            <div className="section section--banner">
+                <div className="container" />
+            </div>
+            {MediumBannerInstance}
+            <div className="section section--ecosystem">
+                <div className="container">
+                    <div className="row">
+                        <div className="section--content">
+                            <ContentBlock subtitle="A Growing Ecosystem">
+                                <p>We're dedicated to developing world class privacy and interoperability focused tools as well as keeping you up-to-date on our progress.</p>
+                            </ContentBlock>
+                        </div>
+                    </div>
                 </div>
-                {MediumBannerInstance}
-                <div className="section section--ecosystem">
+            </div>
+            <Fade up distance="50px" delay={100}>
+                <div className="section section--github">
                     <div className="container">
                         <div className="row">
-                            <div className="section--content">
-                                <ContentBlock subtitle="A Growing Ecosystem">
-                                    <p>We're dedicated to developing world class privacy and interoperability focused tools as well as keeping you up-to-date on our progress.</p>
-                                </ContentBlock>
-                            </div>
+                            <GithubStats limit={6} usernames={["renproject", "republicprotocol"]} />
                         </div>
                     </div>
                 </div>
-                <Fade up distance="50px" delay={100}>
-                    <div className="section section--github">
-                        <div className="container">
-                            <div className="row">
-                                <GithubStats limit={6} usernames={["renproject", "republicprotocol"]} />
-                            </div>
-                        </div>
-                    </div>
-                </Fade>
-                <Fade up distance="50px" delay={100}>
-                    <div className="section section--milestones">
-                        <div className="container">
-                            <div className="row">
-                                <Milestones milestones={getMilestones()} />
-                            </div>
-                        </div>
-                    </div>
-                </Fade>
-                <div className="telegram--banner">
+            </Fade>
+            <Fade up distance="50px" delay={100}>
+                <div className="section section--milestones">
                     <div className="container">
-                        <div className="banner--content">
-                            <div>
-                                <h2>Have your say<img src={speechIcon} /></h2>
-                                <p>Provide input and feedback through our Telegram channel</p>
-                            </div>
-                            <TelegramSelector links={[
-                                { name: "Telegram Community", url: REN_URLS.telegram },
-                                { name: "Telegram Announcements", url: REN_URLS.telegramAnnouncements },
-                            ]} />
+                        <div className="row">
+                            <Milestones milestones={getMilestones()} />
                         </div>
                     </div>
                 </div>
-                <Footer />
+            </Fade>
+            <div className="telegram--banner">
+                <div className="container">
+                    <div className="banner--content">
+                        <div>
+                            <h2>Have your say<img src={speechIcon} /></h2>
+                            <p>Provide input and feedback through our Telegram channel</p>
+                        </div>
+                        <TelegramSelector links={[
+                            { name: "Telegram Community", url: REN_URLS.telegram },
+                            { name: "Telegram Announcements", url: REN_URLS.telegramAnnouncements },
+                        ]} />
+                    </div>
+                </div>
             </div>
-        );
-    }
-}
+            <Footer />
+        </div>
+    );
+};
 
 export default RoadmapPage;
