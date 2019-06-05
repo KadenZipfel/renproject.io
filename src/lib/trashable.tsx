5d4ddd1ad899445c58c5deaaf9a33d018a107cff
Vince Au <vinceau09@gmail.com>
2019-06-05 16:01:14 +1000

    Use utility-types package for Subtract

--- src/lib/trashable.tsx
+++ src/lib/trashable.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+
 import makeTrashable from "trashable";
+
+import { Subtract } from "utility-types";
 
 type TrashablePromise<T> = Promise<T> & { trash: () => void };
 
@@ -14,44 +17,6 @@ interface PromiseStore {
 }
 
 type Key = number;
-
-// type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-/**
- * Subtract
- * @desc From `T` remove properties that exist in `T1` (`T1` is a subtype of `T`)
- * @example
- *   type Props = { name: string; age: number; visible: boolean };
- *   type DefaultProps = { age: number };
- *
- *   // Expect: { name: string; visible: boolean; }
- *   type RestProps = Subtract<Props, DefaultProps>;
- */
-export type Subtract<T extends T1, T1 extends object> = Pick<
-  T,
-  SetComplement<keyof T, keyof T1>
->;
-/**
- * SetComplement
- * @desc Set complement of given union types `A` and (it's subset) `A1`
- * @example
- *   // Expect: "1"
- *   SetComplement<'1' | '2' | '3', '2' | '3'>;
- */
-export type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
-
-
-/**
- * SetDifference (same as Exclude)
- * @desc Set difference of given union types `A` and `B`
- * @example
- *   // Expect: "1"
- *   SetDifference<'1' | '2' | '3', '2' | '3' | '4'>;
- *
- *   // Expect: string | number
- *   SetDifference<string | number | (() => void), Function>;
- */
-export type SetDifference<A, B> = A extends B ? never : A;
-
 
 export function withTrashable<P extends TrashableReactProps>(WrappedComponent: React.ComponentType<P>) {
   return class TrashableClass extends React.Component<Subtract<P, TrashableReactProps>> {
