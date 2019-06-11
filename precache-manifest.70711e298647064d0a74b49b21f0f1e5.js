79bb26a2a90ea2521d121d742950700b91644ba5
Vince Au <vinceau09@gmail.com>
2019-06-11 16:28:04 +1000

    Deploying to renproject.io

---
+++ precache-manifest.70711e298647064d0a74b49b21f0f1e5.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "64050f49d4c42fcd8a3d2df99450ece4",
+    "url": "/static/media/icon-renvm-smpc.64050f49.svg"
+  },
+  {
+    "revision": "8e76a1255e77e4ff4f6e",
+    "url": "/static/css/main.b2152ec3.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b07083b533464c72005c",
+    "url": "/static/js/2.be11c328.chunk.js"
+  },
+  {
+    "revision": "2f693b416267413efa97a304ed43581c",
+    "url": "/static/media/icon-feat-faster.2f693b41.svg"
+  },
+  {
+    "revision": "ec72c97b0ab417fca8f8eeaaa6341d82",
+    "url": "/static/media/icon-feat-interop.ec72c97b.svg"
+  },
+  {
+    "revision": "099de461286870cb6169f9282ea527c5",
+    "url": "/static/media/icon-feat-security.099de461.svg"
+  },
+  {
+    "revision": "23bbcc7c5c5dfed983cb11fce434b99c",
+    "url": "/static/media/more-questions-icon.23bbcc7c.svg"
+  },
+  {
+    "revision": "94b1f336e6e4e022472bdd734ca40bc3",
+    "url": "/static/media/looking-for-help-icon.94b1f336.svg"
+  },
+  {
+    "revision": "9d9ad777bb62a8d4a3207b4e60446cd9",
+    "url": "/static/media/icon-inf-counter.9d9ad777.svg"
+  },
+  {
+    "revision": "eb86f0f906bcca8ce0eeb6547d9c9e8f",
+    "url": "/static/media/icon-inf-lending.eb86f0f9.svg"
+  },
+  {
+    "revision": "3258ea8ba2ab47207890b87a42f47b99",
+    "url": "/static/media/icon-inf-pair.3258ea8b.svg"
+  },
+  {
+    "revision": "61cf01452ee6ed98dbef3a71646aaf1a",
+    "url": "/static/media/gh-icon-star.61cf0145.svg"
+  },
+  {
+    "revision": "650c3580690a7132e0fa7ff6ccb4a289",
+    "url": "/static/media/gh-icon-law.650c3580.svg"
+  },
+  {
+    "revision": "6db89f5ae1e3cb3b6c5ecd8cfe79939b",
+    "url": "/static/media/icon-speech.6db89f5a.svg"
+  },
+  {
+    "revision": "f31cbb9f1f5661a409f4604fef5adb86",
+    "url": "/static/media/icon-docs.f31cbb9f.svg"
+  },
+  {
+    "revision": "fe4b8d8caabe2fd3d234999397da7d0c",
+    "url": "/static/media/icon-renvm-bft.fe4b8d8c.svg"
+  },
+  {
+    "revision": "fffa59dfa302610613e0f2021d24aaa6",
+    "url": "/static/media/icon-renvm-hyperdrive.fffa59df.svg"
+  },
+  {
+    "revision": "21edf9930892f061a1b1986709c71f73",
+    "url": "/static/media/icon-renvm-shamir.21edf993.svg"
+  },
+  {
+    "revision": "8e76a1255e77e4ff4f6e",
+    "url": "/static/js/main.759bdf4f.chunk.js"
+  },
+  {
+    "revision": "bffb3b7aca9b89ef7609f0f6c9686a7d",
+    "url": "/static/media/illustration-about_header.bffb3b7a.svg"
+  },
+  {
+    "revision": "a034d0b94795623d28b464f7bbec4b2b",
+    "url": "/static/media/grey-oval.a034d0b9.svg"
+  },
+  {
+    "revision": "4b46eca471a442db4ead71f332a3c07d",
+    "url": "/static/media/city2.4b46eca4.svg"
+  },
+  {
+    "revision": "64aef44ba20f75e0dbc244dab5db3664",
+    "url": "/static/media/illustration-header.64aef44b.svg"
+  },
+  {
+    "revision": "7bf214f5e7e348a512c3d03deae4cb84",
+    "url": "/static/media/illustration-renex.7bf214f5.svg"
+  },
+  {
+    "revision": "5f775a47996f8d520d8015b8aaac4146",
+    "url": "/static/media/logo.5f775a47.svg"
+  },
+  {
+    "revision": "a6830241555b63ca30e9260e781673f6",
+    "url": "/static/media/logo-wordmark.a6830241.svg"
+  },
+  {
+    "revision": "4299a6f9ef6351f541a1e9be70b93c78",
+    "url": "/static/media/roadmap-banner.4299a6f9.svg"
+  },
+  {
+    "revision": "cfebebe312796c7b36b2b3e5c3109d99",
+    "url": "/static/media/renvm-circle-lines.cfebebe3.svg"
+  },
+  {
+    "revision": "4bd7f63ccf4d345b9265b8feea2e748d",
+    "url": "/static/media/illustration-ren_token.4bd7f63c.svg"
+  },
+  {
+    "revision": "a95d44d70a47068ad70c96139c4f8fbe",
+    "url": "/static/media/SuisseIntl-Thin.a95d44d7.otf"
+  },
+  {
+    "revision": "55790fe1a97bfeba0ef289fd99549b23",
+    "url": "/static/media/SuisseIntl-Book.55790fe1.otf"
+  },
+  {
+    "revision": "ad716f22b3c5a0c6bf6837e9bbafadc4",
+    "url": "/static/media/SuisseIntl-Light.ad716f22.otf"
+  },
+  {
+    "revision": "3f16c813c5c1b5ebe4fb8f1e53eb57c6",
+    "url": "/static/media/SuisseIntl-Regular.3f16c813.otf"
+  },
+  {
+    "revision": "ceda09c978023f0b029310993b9f18c5",
+    "url": "/static/media/SuisseIntl-Bold.ceda09c9.otf"
+  },
+  {
+    "revision": "bf540a0fcd9ccff7af1e960a046cfc6d",
+    "url": "/static/media/SuisseIntl-Black.bf540a0f.otf"
+  },
+  {
+    "revision": "9af7a99c2579a07d4cac96c790216892",
+    "url": "/static/media/illustration-buildingblocks.9af7a99c.png"
+  },
+  {
+    "revision": "2258e0589904b122525b9f5a398ba0ab",
+    "url": "/static/media/illustration-chip.2258e058.png"
+  },
+  {
+    "revision": "7dda5bc38e941b26a6a88a2948dd86bd",
+    "url": "/index.html"
+  }
+];
